c9c047d05a79289f63e41524b36f847f2c989685
kowada.masayuki <masayuki.kowada@kddi-cf.com>
2017-09-28 11:01:24 +0900

    first commit

---
+++ front-page.php
@@ -0,0 +1,36 @@
+<?php get_header(); ?>
+
+<div class="header">
+	<ul class="flex">
+		<li class="site-title"><a href="<?php bloginfo('url'); ?>">sample.com</a></li>
+	</ul>
+</div>
+
+<div class="front-page">
+	<!-- main-visual -->
+	<div class="main-visual">
+		<div class="inner">
+			
+		</div>
+	</div>
+	<!-- //main-visual -->
+	<ul class="front-menu-list">
+		<li class="menu-01">
+			<a href="#">AAA</a>
+		</li>
+		<li class="menu-02">
+			<a href="#">BBB</a>
+		</li>
+		<li class="menu-03">
+			<a href="#">CCC</a>
+		</li>
+	</ul>
+</div>
+
+<div class="footer">
+	<div class="inner">
+		<p>copyright &copy; sample</p>
+	</div>
+</div>
+
+<?php get_footer(); ?>
